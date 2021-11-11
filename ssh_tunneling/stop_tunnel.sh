a09285bcb2731fe7c4538f8fe2168058b78a05f3
Anthony DiFrancesco <acdifran@gmail.com>
2021-11-11 14:06:03 -0800

    remove tunnel, added to zshconfig

--- ssh_tunneling/stop_tunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MASTER_IP=$1
-
-ssh -S aries-socket -O exit ec2-user@$MASTER_IP
-ssh -S cortex-socket -O exit ec2-user@$MASTER_IP
-ssh -S um-socket -O exit ec2-user@$MASTER_IP
-ssh -S gemini-socket -O exit ec2-user@$MASTER_IP
-ssh -S remote-job-gateway-socket -O exit ec2-user@$MASTER_IP
